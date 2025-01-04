1329bfae202a70f1f6a700729b1202634ccbe2e5
nikodyulger <nikolasvdulgerov@gmail.com>
2025-01-04 12:46:22 +0100

    Variables de configuración

---
+++ settings.py
@@ -0,0 +1,15 @@
+PAGE_TITLE = "Portfolio | Nikola Dyulgerov"
+PAGE_ICON = ":clipboard:"
+NAME = "Nikola Dyulgerov"
+DESCRIPTION = """
+Ingeniero informático con interes en aplicaciones serverless, lago de datos y modelos de aprendizaje automático
+"""
+EMAIL = "nikolasvdulgerov@gmail.com"
+SOCIAL_MEDIA = {
+    "LinkedIn": "",
+    "GitHub": "",
+    "Kaggle": ""
+}
+
+PDF_PATH = "static/cv.pdf"
+PROFILE_PIC_PATH = "static/profile.png"

2e1efd9a09c8139949c7dc918bd2b5f4701d55c9
nikodyulger <nikolasvdulgerov@gmail.com>
2026-01-22 00:41:38 +0100

    añadidos proyectos de notebook y calidad de datos albacete

--- pages/3_🛠️_Proyectos.py
+++ pages/3_🛠️_Proyectos.py
@@ -4,13 +4,54 @@ import streamlit as st
 PAGE_TITLE = "Proyectos"
 PAGE_ICON = "🛠️"
 PROJECTS = {
+    "Datos Calidad Aire Albacete": {
+        "link": "https://github.com/nikodyulger/dataeng-ab-aire",
+        "description": """
+            Recopilación de los datos provenientes de las estaciones meteorológicas repartidas por la ciudad de Albacete. Todo se orquesta a través de Airflow. La intención es publicar los datos y documentar todo el proceso de desarrollo
+        """,
+        "tags": [
+            "Airflow",
+            "Polars",
+            "playwright",
+            "Medallion",
+            "Dremio",
+            "Medium",
+        ],
+        "img": "static/dataeng_ab_aire.png",
+    },
+    "Notebooks": {
+        "link": "https://github.com/nikodyulger/kaggle_notebooks",
+        "description": """
+            Recopilación de notebooks de ciencia de datos sobre diferentes temas:
+             - Extracción de datos de APIs públicas sobre los precios de las gasolineras de Albacete y su análisis
+             - Redes neuronales para clasificar imágenes de mariposas
+             - Modelos de clasificación y regresión
+        """,
+        "tags": [
+            "Pandas",
+            "Sci-kit Learn",
+            "Machine Learning",
+            "Neural Networks",
+            "EDA",
+            "Kaggle",
+        ],
+        "img": "static/kaggle_notebooks.png",
+    },
     "Blog Recetas": {
         "link": "https://github.com/nikodyulger/blog-recetas",
         "description": """
             Breve recopilatorio de recetas desplegado en AWS como página web estática que se actualiza con cada push de código
         """,
-        "tags": ["GitHub Actions", "Hugo", "Terraform", "Cloudfront", "S3", "Lambda@Edge", "Route 53"],
-        "img": "static/blog-recetas.png"
+        "tags": [
+            "GitHub Actions",
+            "Hugo",
+            "Terraform",
+            "Cloudfront",
+            "S3",
+            "Lambda@Edge",
+            "Route 53",
+        ],
+        "img": "static/blog-recetas.png",
     },
     "Frigorífico AWS": {
         "link": "https://github.com/nikodyulger/aws-fridge-app",
@@ -18,8 +59,19 @@ PROJECTS = {
             Aplicación para ilustrar cómo desplegar una aplicación web con un pipeline de CI/CD, manejar los costes
             apagando y levantando los contenedores, todo ello utilizando herramientas de infraestructura como código
         """,
-        "tags": ["Cloudformation", "Lambda", "CodePipeline", "AppRunner", "SNS", "DynamoDB", "Route53", "Flask", "Pytest", "Docker"],
-        "img": "static/aws-fridge-app.gif"
+        "tags": [
+            "Cloudformation",
+            "Lambda",
+            "CodePipeline",
+            "AppRunner",
+            "SNS",
+            "DynamoDB",
+            "Route53",
+            "Flask",
+            "Pytest",
+            "Docker",
+        ],
+        "img": "static/aws-fridge-app.gif",
     },
     "Datathon Logic": {
         "link": "https://github.com/nikodyulger/datathon-logic",
@@ -27,7 +79,7 @@ PROJECTS = {
             Proyecto de visualización de datos, nuestra propuesta para el reto del Datathon Cajamar UniversityHack 2022
         """,
         "tags": ["Plotly", "Pandas", "Dash", "Spacy"],
-        "img": "static/datathon.gif"
+        "img": "static/datathon.gif",
     },
     "Interprice": {
         "link": "https://github.com/nikodyulger/interprice",
@@ -35,7 +87,7 @@ PROJECTS = {
             Comparador de precios de productos alimenticios de diferentes supermercados de España
         """,
         "tags": ["Scrapy", "VueJs", "Lambda", "Aurora MySQL", "API Gateway", "S3"],
-        "img": "static/interprice.gif"
+        "img": "static/interprice.gif",
     },
 }
 
@@ -46,7 +98,7 @@ st.divider()
 
 
 for pr in PROJECTS.keys():
-    col1, col2 = st.columns([5,1])
+    col1, col2 = st.columns([5, 1])
     with col1:
         st.subheader(pr)
     with col2:
